c4acd0f5e3b13585312562d8b90a1f455acbc98d
halisonyasha@gmail.com <halisonyasha@gmail.com>
2025-06-11 20:24:51 -0300

    .

--- src/db/data-source-cli.ts
+++ src/db/data-source-cli.ts
@@ -1,15 +1,15 @@
 import 'dotenv/config';
-import { DataSource, DataSourceOptions } from "typeorm";
+import { DataSource, DataSourceOptions } from 'typeorm';
 
 const dataSourceOptions: DataSourceOptions = {
-    type: 'postgres',
-    host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT),
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    entities: [__dirname + '/../**/*.entity.{js,ts}'], //acessa qualquer arquivo que tenha o .entity dentro de src, colocando dessa maneira para não colocar cada entity criada
-    migrations: [__dirname + '/migrations/*.{js,ts}'],
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  entities: [__dirname + '/../**/*.entity.{js,ts}'],
+  migrations: [__dirname + '/migrations/*.{js,ts}'],
 };
 
 const dataSource = new DataSource(dataSourceOptions);
